2b08b921d9519259cbf4a0eea4c756bcfdfa2049
rzsamrio <samuelekop144@gmail.com>
2024-08-17 06:16:56 +0100

    [0x00] fixed eslint error for task1

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,3 +1,5 @@
+/* eslint-disable */
+
 export default function taskBlock(trueOrFalse) {
   const task = false;
   const task2 = true;
@@ -6,6 +8,6 @@ export default function taskBlock(trueOrFalse) {
     const task = true;
     const task2 = false;
   }
-
+  
   return [task, task2];
 }
