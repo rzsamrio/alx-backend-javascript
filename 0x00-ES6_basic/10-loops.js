344a12c4b8a40d20aeb4ccb792846b483bf1e896
rzsamrio <samuelekop144@gmail.com>
2024-09-03 19:02:00 +0100

    [0x00] 10: hm

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,6 +1,6 @@
 export default function appendToEachArrayValue(array, appendString) {
-  for (const [idx, values] of array.entries()) {
-    array[idx] = appendString + values;	/* eslint-disable-line */
+  for (let value of array) {
+    value = appendString + value;
   }
 
   return array;
