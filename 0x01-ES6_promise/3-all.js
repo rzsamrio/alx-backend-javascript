34ca721088c1eb6dd28c9b03f1932f04d02d6e14
rzsamrio <samuelekop144@gmail.com>
2024-11-05 19:39:52 +0100

    [0X01] Task 3 fix

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -3,7 +3,7 @@ import { uploadPhoto, createUser } from './utils';
 export default function handleProfileSignup() {
   Promise.all([uploadPhoto(), createUser()]).then((info) => {
     console.log(`${info[0].body} ${info[1].firstName} ${info[1].lastName}`);
+  }).catch(() => {
+    console.log('Signup system offline');
   });
 }
-
-handleProfileSignup();
