19bb658c3abd8fc4a3c15482feb912359fae42c6
rzsamrio <samuelekop144@gmail.com>
2024-11-06 10:38:12 +0100

    [0x09] task 9

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -3,7 +3,7 @@ export default function guardrail(mathFunction) {
   try {
     queue.push(mathFunction());
   } catch (error) {
-    queue.push(error);
+    queue.push(`Error: ${error.message}`);
   }
   queue.push('Guardrail was processed');
 
