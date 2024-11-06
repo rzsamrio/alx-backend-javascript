4fb2cbc0b13edb4004375110be87b6226f1f1229
rzsamrio <samuelekop144@gmail.com>
2024-11-06 17:52:36 +0100

    [0x02] add task 4

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,40 @@
+/* eslint-disable */
+
+import Currency from './3-currency';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this.amount = amount;
+    this.currency = currency;
+  }
+
+  set amount(value) {
+    if (typeof value !== 'number') {
+      throw TypeError('Amount must be a number');
+    }
+    this._amount = value;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set currency(value) {
+    if (!(value instanceof Currency)) {
+      throw TypeError('Currency must be a currency');
+    }
+    this._currency = value;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  displayFullPrice() {
+    return `${this.amount} ${this.currency.name} (${this.currency.code})`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
+}
