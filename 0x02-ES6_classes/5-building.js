bd286206582e76f6810e2e3866c609ba68b87b4a
rzsamrio <samuelekop144@gmail.com>
2024-11-08 11:42:44 +0100

    [0x02] D: Task 2: Not string

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,7 +1,7 @@
 /* eslint-disable */
 export default class Building {
   constructor(sqft) {
-    if (new.target !== 'Building' && typeof this.evacuationWarningMessage !== 'function') {
+    if (new.target !== Building && typeof this.evacuationWarningMessage !== 'function') {
       throw Error('Class extending Building must override evacuationWarningMessage');
     }
     this._sqft = sqft;
