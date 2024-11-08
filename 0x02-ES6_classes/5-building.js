65263ca944acee698bbba945f002d8c48c1810d3
rzsamrio <samuelekop144@gmail.com>
2024-11-08 10:27:56 +0100

    [0x02] add task 5

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,13 @@
+/* eslint-disable */
+export default class Building {
+  constructor(sqft) {
+    if (new.target !== 'Building' && typeof this.evacuationWarningMessage !== 'function') {
+      throw Error('Class extending Building must override evacuationWarningMessage');
+    }
+    this._sqft = sqft;
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+}
